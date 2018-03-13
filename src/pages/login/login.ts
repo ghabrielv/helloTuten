0a61b60653c2bdc7108032a6b12e1fecfc9d5d1f
Ghabriel Villarreal <ghabrielv@gmail.com>
2018-03-13 17:28:12 -0400

    Obtener token de sesión

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -12,10 +12,12 @@ import { HomePage } from '../home/home';
 export class LoginPage {
   responseData : any;
   userData = {"email" : "", "password" : "", "app" : "APP_WEB"};
+  userAPI = {"email" : "testapis@tuten.cl", "password" : "1234", "app" : "APP_BCK"};
   LoginForm: FormGroup;
 
   constructor(public navCtrl: NavController, public LoginServiceProvider:LoginServiceProvider, public fb: FormBuilder) {
     if (typeof(localStorage.getItem("userData")) == 'string') {
+      this.obtenerToken();
       this.navCtrl.push(HomePage);
     }
     this.LoginForm = this.createForm();
@@ -26,6 +28,14 @@ export class LoginPage {
       this.responseData = result;
       localStorage.setItem('userData', JSON.stringify(this.responseData));
       this.navCtrl.push(HomePage);
+    }, (err) => {
+    });
+  }
+
+  obtenerToken() {
+    this.LoginServiceProvider.LoginFromApi(this.userAPI.email, this.userAPI.password, this.userAPI.app).then((result) => {
+      this.responseData = result;
+      localStorage.setItem('userAPI', JSON.stringify(this.responseData));
     }, (err) => {
     });
   }
