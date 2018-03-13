f2b7b426e5741a14f03dbcd4c15e830e5493a726
Ghabriel Villarreal <ghabrielv@gmail.com>
2018-03-13 17:29:11 -0400

    Obtener bookingData

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { LoginServiceProvider } from '../../providers/login-service/login-service';
+import { BookingServiceProvider } from '../../providers/booking-service/booking-service';
+
 import { LoginPage } from '../login/login';
 
 @Component({
@@ -9,14 +12,28 @@ import { LoginPage } from '../login/login';
 })
 export class HomePage {
   public userData : any;
+  public userAPI: any;
+  public bookingData: any;
+  responseData : any;
 
-  constructor(public navCtrl: NavController) {
-  	this.userData = JSON.parse(localStorage.getItem('userData'));
+  constructor(public navCtrl: NavController, public BookingServiceProvider:BookingServiceProvider) {
+    this.userData = JSON.parse(localStorage.getItem('userData'));
+    this.userAPI = JSON.parse(localStorage.getItem('userAPI'));
+    this.obtenerLista();
   }
 
   logout(){
     localStorage.clear();
     setTimeout(() => this.navCtrl.push(LoginPage), 1000);
+  }
+
+  obtenerLista(){
+    this.BookingServiceProvider.BookingFromApi("testapis@tuten.cl", "miguel@tuten.cl", true, this.userAPI.sessionTokenBck, "APP_BCK").then((result) => {
+      this.responseData = result;
+      localStorage.setItem('bookingData', JSON.stringify(this.responseData));
+      this.bookingData = JSON.parse(localStorage.getItem('bookingData'));
+    }, (err) => {
+    });
   }
 
 }
